a1182aaeeefb40db49c7d38c251fdfc6e80eb7aa
irinashipounova <ira9ship@gmail.com>
2022-06-30 14:33:38 +0300

    Мария, добрый день! А как я пойму, что Вы увидели эти файлы и, возможно, что-то мне написали в ответ?Initial commit

---
+++ p_4_1.py
@@ -0,0 +1,7 @@
+from sys import argv
+
+name, time, rate, enc = argv
+
+print(int(time) * int(rate) + int(enc))
+
+#вызывала программу через терминал:  C:\Users\User\PycharmProjects\pythonProject4> python p_4_1.py 48 5 50
